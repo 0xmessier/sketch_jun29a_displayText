02963efcf684f59557f18f0e9f2404322b955b9f
0xmessier <kayasoft61@gmail.com>
2023-07-02 15:09:24 +0300

    file

---
+++ sketch_jun29a.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal.h> 
+
+int Contrast = 100;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  
+
+void setup()
+{
+  analogWrite(6,Contrast);
+  lcd.begin(16, 2);
+} 
+  
+void loop()
+{ 
+  lcd.setCursor(0, 0);
+  lcd.print("IN CODE WE TRUST");
+
+  lcd.setCursor(0, 1);
+  lcd.print("      M87");
+}
